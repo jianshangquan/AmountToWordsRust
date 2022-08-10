b24fdb0b018a9dbdd51bd9211200c36a70c58dda
jianshangquan <jianshangquan3@gmail.com>
2022-08-10 18:04:37 +0630

    v0.0.1

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use amount_to_words::AmountConversion;
 fn main() {
     let amt:u128 = 1234567899;
     let result = AmountToWords::convertTo(&12346500000, AmountConversion::Burmese);
-    print!("{}", result);
+    // print!("{}", result);
 }
 
 
